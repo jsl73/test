211f4fa9448333853541ee3a71693bda4920dee4
jsl73 <jonas.sondell@gmail.com>
2016-06-01 20:58:36 +0200

    Julia

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 //Hello.java
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, world from GIT!");
+		System.out.println("Hello, world from Julia!");
 		// System.out.println("Hello after last commit!!!");
 	}
 }
