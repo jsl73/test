cbd4ff167b3be781a87dfa2faa2961bda76affdc
jsl73 <jonas.sondell@gmail.com>
2016-06-01 21:49:47 +0200

    Test av GIT i netbeans

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 //Hello.java
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, world from Julia!");
+		System.out.println("Hello, world from Julia and dad!");
 		//System.out.println("Hello after last commit!!!");
 	}
 }
