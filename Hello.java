74ef1a8ceef4cff44c438ecc1666b51ee7da323d
jsl73 <jonas.sondell@gmail.com>
2016-06-01 20:06:47 +0200

    Third commit.

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
-		//System.out.println("Hello after last commit!!!");
+		// System.out.println("Hello after last commit!!!");
 	}
 }
 
