0aa168665277dace0863fa7a14f6dc4bdb30b782
jsl73 <jonas.sondell@gmail.com>
2016-06-15 15:24:00 +0200

    New editor, new test

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from Julia and dad!");
 		System.out.println("Hello after the very last commit!!!");
+		System.out.println("IntelliJ test");
 	}
 }
 
